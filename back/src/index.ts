5f603210cadc9dd919798608f8c7a378e76f7bff
Anthony <anthonylopes037@gmail.com>
2025-12-24 15:28:46 +0100

    fix: cors backend

--- back/src/index.ts
+++ back/src/index.ts
@@ -11,25 +11,31 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// Middlewares
 app.use(cors({
   origin: [
     'http://localhost:5173',
     "https://rental-app-1e1u.onrender.com"
-  ]
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
+
+app.options('*', cors()); 
+
+
 app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/apartments', apartmentRoutes);
 app.use('/api/tenants', tenantRoutes);
-app.use('/api/pdf', pdfRoutes)
+app.use('/api/pdf', pdfRoutes);
 
 app.get('/health', (req, res) => {
   res.json({ status: "OK", timestamp: new Date() });
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Serveur Gestion Locative prêt sur http://localhost:${PORT}`);
+  console.log(`🚀 Serveur prêt sur le port ${PORT}`);
 });
